ebb36c4c1f4e815e3ea15b34e3b2bccdc5e42708
Charlie Guenther <guen0032@algonquinlive.com>
2023-06-11 15:17:37 -0400

    base styles & setup

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-	return src('shinobi/**/*.scss').pipe(sass()).pipe(dest('css'));
+	return src('dns/**/*.scss').pipe(sass()).pipe(dest('css'));
 }
 
 function watchTask() {
-	watch(['shinobi/**/*.scss'], buildStyles);
+	watch(['dns/**/*.scss'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
